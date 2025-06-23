43f0cd79a13de09ac471cb6e644ef52100dcb6b9
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-23 18:58:42 -0300

    Feat: atributos da classe User criados

---
+++ dscommerce/src/main/java/com/jeffmmartins/dscommerce/entities/User.java
@@ -0,0 +1,14 @@
+package com.jeffmmartins.dscommerce.entities;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private long id;
+    private String name;
+    private String phone;
+    private String email;
+    private LocalDate birthDate;
+    private String password;
+    private String roles;
+}
