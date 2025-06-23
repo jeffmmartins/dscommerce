7fb5983163d9b352474a6287ec2ec7c6c4072c57
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-23 18:48:26 -0300

    Feat: paconte etentities criado

---
+++ dscommerce/src/main/java/com/jeffmmartins/dscommerce/DscommerceApplication.java
@@ -0,0 +1,14 @@
+package com.jeffmmartins.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}
---
+++ dscommerce/src/test/java/com/jeffmmartins/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffmmartins.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
