74bcadf938d069935bb2b54617e7d5c6428f7ef8
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-25 15:13:58 -0300

    Feat: class User criada

--- dscommerce/src/main/java/com/jeffmmartins/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/jeffmmartins/dscommerce/entities/User.java
@@ -10,5 +10,63 @@ public class User {
     private String email;
     private LocalDate birthDate;
     private String password;
-    private String roles;
+
+    public User(){}
+
+    public User(long id, String name, String phone, String email, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
