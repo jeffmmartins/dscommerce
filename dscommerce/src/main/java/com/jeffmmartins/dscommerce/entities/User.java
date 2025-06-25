8cbb457d3d3e2fc3010f9f5dcb3cb6f3f9612056
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-25 15:40:17 -0300

    Feat: Mapeamento da classe user realizado

--- dscommerce/src/main/java/com/jeffmmartins/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/jeffmmartins/dscommerce/entities/User.java
@@ -1,9 +1,15 @@
 package com.jeffmmartins.dscommerce.entities;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String phone;
